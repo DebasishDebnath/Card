3d9adb3a857c682b13a92d3d39ab1812482665dc
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-03-13 11:00:30 +0530

    Action Card

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function FancyCard() {
   return (
     <View>
-      <Text style={styles.headingText}>TrndingPlaces</Text>
+      <Text style={styles.headingText}>Trnding Places</Text>
       <View style={[styles.card, styles.cardElement]}>
         <Image
           source={{
@@ -26,23 +26,57 @@ export default function FancyCard() {
 }
 
 const styles = StyleSheet.create({
-  headingText: {},
-  card: {},
-  cardElement: {},
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 30,
+  },
+  cardElement: {
+    backgroundColor: '#FFFFFF',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
   cardImage: {
     height: 180,
-    // width: 280,
+    marginBottom: 8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
   },
   cardBody: {
-    alignItems: 'center',
-
-    padding: 15,
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
   },
   cardTitle: {
+    color: '#000000',
+    fontSize: 22,
     fontWeight: 'bold',
-    fontSize: 20,
+    marginBottom: 4,
   },
-  cardLabel: {},
-  cardDescription: {},
-  cardFooter: {},
+  cardLabel: {
+    color: '#000000',
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  cardDescription: {
+    color: '#FF6666',
+    fontSize: 12,
+    marginBottom: 12,
+    marginTop: 6,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#000000',
+  },
 });
